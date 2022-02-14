97a838b66e87994d1bef80c255162100174f8d46
Abel Priego Borrego <apriegob@emeal.nttdata.com>
2022-02-14 09:44:30 +0100

    Header con imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [ AppComponent, PrincipalComponent] //se dice cual es el componente principal, se ha cambiado del AppComponente al PrincipalComponent
+  bootstrap: [PrincipalComponent] //se dice cual es el componente principal, se ha cambiado del AppComponente al PrincipalComponent
 })
 export class AppModule { }
